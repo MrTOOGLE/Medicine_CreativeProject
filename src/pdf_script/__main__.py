4fda664e358b2b367ca3c8a06c5eddd6f9bc0232
MrTOOGLE <Leva.mokrov@gmail.com>
2023-03-19 17:20:30 +0700

    v1.01

--- src/pdf_script/__main__.py
+++ src/pdf_script/__main__.py
@@ -5,11 +5,11 @@ from environment import AppEnvironment
 def main():
     AppEnvironment.configure()
 
-    # for pdf_path in AppEnvironment.get_pdf_paths():
-    #     PDFHandler(pdf_path).extract_images()
-
     for pdf_path in AppEnvironment.get_pdf_paths():
-        PDFHandler(pdf_path).extract_text()
+        PDFHandler(pdf_path).extract_images()
+
+    # for pdf_path in AppEnvironment.get_pdf_paths():
+    #     PDFHandler(pdf_path).extract_text()
 
 
 if __name__ == '__main__':
