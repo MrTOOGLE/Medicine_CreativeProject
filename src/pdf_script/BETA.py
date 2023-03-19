8decbb53dc6936db4de5aaebead706ca3196344a
MrTOOGLE <Leva.mokrov@gmail.com>
2023-03-19 18:41:30 +0700

    v1.043

--- src/pdf_script/BETA.py
+++ src/pdf_script/BETA.py
@@ -1,8 +1,11 @@
 from src.pdf_script.environment import AppEnvironment
+import fitz
 
-with open(AppEnvironment.TEXT_PATH + '/1/1.txt') as f:
-    for i in f:
-        if "fig." in i.lower():
-            print(i)
-
-print("ALSDMAJSDNAJSDNAJSDNjASd")
+doc = fitz.open(AppEnvironment.PDF_FILES_PATH + '1.pdf')
+for page in doc:
+    blocks = page.get_text("dict", flags=11)["blocks"]
+    for b in blocks:
+        for l in b["lines"]:
+            for s in l["spans"]:
+                print(f'{s["text"]} - {s["size"]}')
+    print()
