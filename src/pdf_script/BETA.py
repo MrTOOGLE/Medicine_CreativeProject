04ac50659e5c7c3260a6e0cd03a65c48a65c0bd8
MrTOOGLE <Leva.mokrov@gmail.com>
2023-03-24 21:47:26 +0700

    v1.046

--- src/pdf_script/BETA.py
+++ src/pdf_script/BETA.py
@@ -1,14 +1,8 @@
-from environment import AppEnvironment
-import fitz
+from string import ascii_lowercase
 
-doc = fitz.open(AppEnvironment.PDF_FILES_PATH + '1.pdf')
-for page in doc:
-    blocks = page.get_text("dict", flags=11)["blocks"]
-    for block in blocks:
-        text = ""
-        for line in block["lines"]:
-            for span in line["spans"]:
-                if span["size"] == 8.5:
-                    text += span["text"]
-        print(text)
-
+s = ""
+result = 0
+for i in ascii_lowercase:
+    if f'({i})' in s:
+        result += 1
+print(result)
