0b04b687d229f07591eb692fdc0f0cf8e7c24800
MrTOOGLE <Leva.mokrov@gmail.com>
2023-05-21 14:11:16 +0700

    v1.063.1

---
+++ src/neural_training/Sorting_photos.py
@@ -0,0 +1,24 @@
+import os
+import numpy as np
+from keras.models import load_model
+from keras.utils import load_img
+from keras.utils import img_to_array
+
+model = load_model('D:/_Python/Programs/Medicine/src/neural_training/bacterias-25-0.994.hdf5')
+path = 'D:/_Python/Programs/Medicine/resources/images'
+
+for subdir, dirs, files in os.walk(path):
+    for file in files:
+        try:
+            image_path = os.path.join(subdir, file)
+            image_name = image_path.split('\\')[-1]
+
+            image = load_img(image_path, target_size=(180, 180))
+            image = img_to_array(image)
+            image = np.expand_dims(image, axis=0)
+
+            result = model.predict(image)
+            if result[0][0] <= 0.5:
+                os.rename(image_path, f'D:/_Python/Programs/Medicine/resources/images/Bacterias/{image_name}')
+        except OSError:
+            pass
